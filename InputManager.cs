aff91d7d1946f7d3937aced678101064e6597112
Rubatronix94 <133864077+Rubatronix94@users.noreply.github.com>
2025-03-04 00:29:38 +0100

    STARTER ASSTETS__Trasteando

--- InputManager.cs
+++ InputManager.cs
@@ -1,36 +1,61 @@
-using System.Collections; using System.Collections.Generic; 
-using UnityEngine; using UnityEngine.InputSystem; 
 
-public class InputManager : MonoBehaviour
-{
-    private PlayerInputMap playerInput;
-    private PlayerInputMap.OnFootActions onFoot;
-    private PlayerMotor motor;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-    void Awake()
+
+    public class InputManager : MonoBehaviour
     {
-        playerInput = new PlayerInputMap();
-        onFoot = playerInput.OnFoot;
+        private PlayerInputMap playerInput;
+        private PlayerInputMap.OnFootActions onFoot;
 
-        motor = GetComponent<PlayerMotor>();
+        private PlayerLocomotion motor;
+     /*   private EscudoUp escudo; // Referencia al script EscudoUp
+        private MeleeHandler meleeHandler;
 
-        // Registra eventos
-     onFoot.EquiparEspada.performed += ctx => motor.EquiparEspada();
-        
+        // ATAQUES
+        public event System.Action OnAttack1;
+        public event System.Action OnAttack2;
+     */
+
+        private void Awake()
+        {
+            playerInput = new PlayerInputMap();
+            onFoot = playerInput.OnFoot;
+
+            motor = GetComponent<PlayerLocomotion>();
+        /*    escudo = GetComponent<EscudoUp>();
+            meleeHandler = GetComponent<MeleeHandler>();
+
+            if (motor == null || escudo == null || meleeHandler == null)
+            {
+                Debug.LogError("Missing required component(s) on the GameObject.");
+                enabled = false;
+                return;
+            }
+
+            // Configurar eventos de las acciones
+            onFoot.EquiparEspada.performed += ctx => motor.EquiparEspada();
+            onFoot.Bloqueando.performed += ctx => escudo.ActivarEscudo();
+            onFoot.Attack1.performed += ctx => OnAttack1?.Invoke();
+            onFoot.Attack2.performed += ctx => OnAttack2?.Invoke();
+
+
+            onFoot.FijarObjetivo.performed += ctx => motor.FijarObjetivo();
+        */
+        }
+
+        private void FixedUpdate()
+        {
+            motor.ProcessMove(onFoot.Movimiento.ReadValue<Vector2>());
+        }
+
+        private void OnEnable()
+        {
+            onFoot.Enable();
+        }
+
+        private void OnDisable()
+        {
+            onFoot.Disable();
+        }
     }
-
-    void FixedUpdate()
-    {
-       motor.ProcessMove(onFoot.Movimiento.ReadValue<Vector2>());
-    }
-
-    private void OnEnable()
-    {
-        onFoot.Enable();
-    }
-
-    private void OnDisable()
-    {
-        onFoot.Disable();
-    }
-}
